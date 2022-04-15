c821d50a26590c3495df01901cab2ed45b2d3f4f
Anvitha Lakshmisha <98431766+anvithalakshmisha@users.noreply.github.com>
2022-04-15 17:51:24 -0400

    add components

---
+++ web-app/src/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+class NavBar extends React.Component {
+    state = {  } 
+    render() { 
+        return (<h1>Nav bar</h1>);
+    }
+}
+ 
+export default NavBar;
---
+++ web-app/src/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+class SideBar extends React.Component {
+    state = {  } 
+    render() { 
+        return (<h1>Side bar</h1>);
+    }
+}
+ 
+export default SideBar;

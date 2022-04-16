1607a7c5ffc437f7d67cf20773dbbbf2553723e9
Hazel Lobo <lobo.ha@northeastern.edu>
2022-04-16 14:33:29 -0400

    Added Sidebar Changes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,10 +1,25 @@
-import './App.scss';
-import NavBar from './NavBar/NavBar'
-import SideBar from './SideBar/SideBar';
+import "./App.scss";
+// import NavBar from "./NavBar/NavBar";
+import Sidebar from "./Sidebar/Sidebar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { TaskList } from "./Sidebar/TaskList/TaskList";
+import { Reports } from "./Sidebar/Reports/Reports";
+import { Board } from "./Sidebar/Board/Board";
+import { Project } from "./Sidebar/ProjectDetails/ProjectDetails";
 
 function App() {
   return (
-    <><NavBar></NavBar><SideBar></SideBar></>
+    <>
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route path="/tasks" exact component={TaskList} />
+          <Route path="/board" exact component={Board} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/project" component={Project} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 

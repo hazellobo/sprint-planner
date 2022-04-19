588cb2aed152236992c6c4efe953293dba7b455b
Anvitha Lakshmisha <98431766+anvithalakshmisha@users.noreply.github.com>
2022-04-19 19:50:16 -0400

    Revert "add nav bar | integrate ag-grid"
    This reverts commit 8744e975b243186b9efa37d48fd82515afd92eac.
    
    t

--- web-app/src/NavBar/NavBar.jsx
+++ web-app/src/NavBar/NavBar.jsx
@@ -1,57 +1,11 @@
-import React from "react";
-import "./NavBar.scss";
-import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
+import React from 'react';
 
-class NavBarC extends React.Component {
-  state = {};
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            Sprint Planner
-          </Navbar.Brand>
-          <Button variant="primary">Create Ticket</Button>
-          <Navbar.Collapse id="navbarScroll">
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-            <Nav>
-              <NavDropdown title="Profile" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Logout</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+
+class NavBar extends React.Component {
+    state = {  } 
+    render() { 
+        return (<h1>Nav bar</h1>);
+    }
 }
-
-export default NavBarC;
+ 
+export default NavBar;
--- web-app/src/SideBar/SideBar.jsx
+++ web-app/src/SideBar/SideBar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import TaskList from './TaskList/TaskList';
 
 
 class SideBar extends React.Component {
     state = {  } 
     render() { 
-        return (<TaskList></TaskList>);
+        return (<h1>Side bar</h1>);
     }
 }
  

c46991cce797936fc6556d81ca90bcce90d5e064
Hazel Lobo <lobo.ha@northeastern.edu>
2022-04-22 00:10:03 -0400

    Fixed login routing

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -12,65 +12,62 @@ import NavBarC from "./NavBar/NavBar";
 
 function App() {
   const adminUser = {
-    email : "admin@admin.com",
-    password : "admin123"
-  }
-  const [user, setUser] = useState({name : "", email : ""});
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+  const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
-  const Login = details => {
+  const Login = (details) => {
     console.log(details);
 
-    if (details.email == adminUser.email && details.password == adminUser.password)
-    {
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
       console.log("Logged in");
-      setUser(
-        {
-          name:details.name,
-          email:details.email
-        }
-      );
-    } else 
-    {
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
       console.log("Details do not match!");
       setError("Details do not match!");
     }
-  }
-  const Logout = () =>
-  {
+  };
+  const Logout = () => {
     setUser({
-      name : "", email : " "
+      name: "",
+      email: " ",
     });
-  }
+  };
   return (
-    <div className = "App">
-      {
-        (user.email != "") ? (
-          <div className="welcome">\
-          <h2>Welcome, <span>{user.name}</span></h2>
+    <div className="App">
+      {user.email !== "" ? (
+        <div className="main">
+          <NavBarC></NavBarC>
+          <div className="sidebar-routes">
+            <Router>
+              <SideBar />
+              <Switch>
+                <Route path="/tasks" exact component={TaskList} />
+                <Route path="/board" exact component={Board} />
+                <Route path="/reports" component={Reports} />
+                <Route path="/project" component={Project} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </Router>
+          </div>
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+
           <button onClick={Logout}>Logout</button>
-            </div>)
-            :(
-              <LoginForm Login = {Login} error = {error}/>
-            )
-          }
-          )
-          <></>
-    {/* <LoginForm></LoginForm> */}
-    <div className="main">
-      <NavBarC></NavBarC>
-      <div className="sidebar-routes">
-      <Router>
-        <SideBar />
-        <Switch>
-          <Route path="/tasks" exact component={TaskList} />
-          <Route path="/board" exact component={Board} />
-          <Route path="/reports" component={Reports} />
-          <Route path="/project" component={Project} />
-          <Route path = "/login" component={Login}/>
-        </Switch>
-      </Router>
-      </div>
-    </div>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+      )<></>
+      {/* <LoginForm></LoginForm> */}
     </div>
   );
 }
